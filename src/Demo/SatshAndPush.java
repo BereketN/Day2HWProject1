ef47c4cb2b0a300a51d55ae3414ac24bcff53c5e
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-10 09:42:00 -0700

    Day 2 Initial Commit

---
+++ src/Demo/SatshAndPush.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class SatshAndPush {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Day 2 Stash&Push");
+	}
+	
+	public void add(int a, int b) {
+		
+		System.out.println(a - b);
+	}
+}
