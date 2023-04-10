1f778388cba01b59ae4a7877cd8ab5732d035d59
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-10 13:36:56 -0700

    Implemented subtract()

--- src/Demo/SatshAndPush.java
+++ src/Demo/SatshAndPush.java
@@ -2,13 +2,8 @@ package Demo;
 
 public class SatshAndPush {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Day 2 Stash&Push");
-	}
-	
-	public void add(int a, int b) {
-		
-		System.out.println(a - b);
+	public void subtract () {
+
+		System.out.println(1 - 2 - 3);
 	}
 }
